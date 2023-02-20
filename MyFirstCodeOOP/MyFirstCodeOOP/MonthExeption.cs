c5a53d65a79753bb4288d9651fb1fd012febd3fb
Daniel Montoya <danielemilio201@gmail.com>
2023-02-20 13:18:38 -0500

    I created my first code a new Date Class

---
+++ MyFirstCodeOOP/MyFirstCodeOOP/MonthExeption.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace MyFirstCodeOOP
+{
+    [Serializable]
+    internal class MonthExeption : Exception
+    {
+        public MonthExeption()
+        {
+        }
+
+        public MonthExeption(string message) : base(message)
+        {
+        }
+
+        public MonthExeption(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected MonthExeption(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
---
+++ MyFirstCodeOOP/MyFirstCodeOOP/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyFirstCodeOOP
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var dateObject = new Date(1800, 2, 15);
+                Console.WriteLine(dateObject);
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+        }
+    }
+}
