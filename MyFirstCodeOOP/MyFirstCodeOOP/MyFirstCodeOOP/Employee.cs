7559c8e786013123e6ed00196a9b91265830ab24
Daniel Montoya <danielemilio201@gmail.com>
2023-02-23 19:23:31 -0500

    Creation new Abstract class called Employee

---
+++ MyFirstCodeOOP/MyFirstCodeOOP/MyFirstCodeOOP/Employee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstCodeOOP
+{
+    public abstract class Employee
+    {
+        #region Properties
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Date BirthDate { get; set; }
+        public Date HiringDate { get; set; }
+        public bool IsActive { get; set; }
+        #endregion
+
+
+        #region Methods
+        public abstract Decimal GetValueToPay();
+        public override string ToString()
+        {
+            return $"-----EMPLOYEE------ \n\t " +
+                $"ID: {Id} \n\t " +
+                $"Fist Name: {FirstName} \n\t " +
+                $"Last Name: {LastName} \n\t " +
+                $"Birht Date: {BirthDate} \n\t " +
+                $"Hiring Dat: {HiringDate}  \n\t " +
+                $"Is Active: {IsActive}";
+        }
+
+
+        #endregion
+    }
+}
