d30d860a6b7e5ec21ebdb6a81a776f3a72999f42
Daniel Montoya <danielemilio201@gmail.com>
2023-02-27 11:37:05 -0500

    Implementation in Program.cs to show the SalaryEmployee Object

--- MyFirstCodeOOP/MyFirstCodeOOP/MyFirstCodeOOP/Program.cs
+++ MyFirstCodeOOP/MyFirstCodeOOP/MyFirstCodeOOP/Program.cs
@@ -8,8 +8,36 @@ namespace MyFirstCodeOOP
         {
             try
             {
-                var dateObject = new Date(1999, 2, 15);
-                Console.WriteLine(dateObject);
+                Console.WriteLine("OOP CONCEPTS \n\t " +
+                    "****************************");
+
+                Console.WriteLine("Please put your year:");
+                var year = Console.ReadLine();
+
+                Console.WriteLine("\n\t Please put your month:");
+                var month = Console.ReadLine();
+
+                Console.WriteLine("\n\t Please put your day:");
+                var day = Console.ReadLine();
+
+                Console.WriteLine(new Date(Convert.ToInt32(year), Convert.ToInt32(month), Convert.ToInt32(day)));
+
+                Console.WriteLine("++++++++++++TESTING LASTET IMPLEMENTATION+++++++++++++++++++");
+
+                Employee salaryEmployee = new SalaryEmployee()
+                {
+                    Id = 100,
+                    FirstName = "Maria",
+                    LastName = "Posada",
+                    BirthDate = new Date(1950, 2, 5),
+                    HiringDate = new Date(2022, 12, 12),
+                    IsActive = true,
+                    Salary = 2000000.34M
+                };
+
+                Console.WriteLine(salaryEmployee);
+
+
             }
             catch (Exception ex)
             {
