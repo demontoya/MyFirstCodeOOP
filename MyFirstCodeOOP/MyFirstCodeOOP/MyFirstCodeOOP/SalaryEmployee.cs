e2d8037bf12fc822ec6987b3a442553f19060351
Daniel Montoya <danielemilio201@gmail.com>
2023-02-27 10:49:25 -0500

    Creating a new class concret called SalaryEmployee

--- MyFirstCodeOOP/MyFirstCodeOOP/MyFirstCodeOOP/SalaryEmployee.cs
+++ MyFirstCodeOOP/MyFirstCodeOOP/MyFirstCodeOOP/SalaryEmployee.cs
@@ -4,7 +4,25 @@ using System.Text;
 
 namespace MyFirstCodeOOP
 {
-    internal class SalaryEmployee
+    public class SalaryEmployee : Employee
     {
+        #region Properties
+        public decimal Salary { get; set; }
+
+        #endregion
+
+        #region Methods
+        public override decimal GetValueToPay()
+        {
+            return Salary;  
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()} \n\t " +
+                $"Value to Pay:{GetValueToPay()}";
+        }
+
+        #endregion
     }
 }
